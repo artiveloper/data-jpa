2dbfeecf11f67d629066080d799436850e194e89
ravi <artiveloper@gmail.com>
2020-10-27 13:54:55 +0900

    프로젝트 환경설정

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,26 @@
+package study.datajpa.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+}
